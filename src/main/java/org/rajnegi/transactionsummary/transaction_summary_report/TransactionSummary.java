8058f9c98a8c127fbd2c09e992a237af47ea056f
Rajeev Negi <RajeevNegi@Rajeevs-MBP.gateway>
2018-08-26 03:37:24 +1000

    Modified

--- src/main/java/org/rajnegi/transactionsummary/transaction_summary_report/TransactionSummary.java
+++ src/main/java/org/rajnegi/transactionsummary/transaction_summary_report/TransactionSummary.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -64,7 +65,7 @@ public class TransactionSummary {
 			System.exit(1);
 		}
 		
-		return null;
+		return Collections.<TransactionRecordBean>emptyList();
 	}
 
 }
